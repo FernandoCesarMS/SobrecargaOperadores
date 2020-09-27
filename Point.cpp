199c618d9279a89a797b84980cb4042b05c92a68
Fernando Cesar <cmnando.silva@gmail.com>
2020-09-27 00:29:31 -0300

    Point::operator + feito

--- Point.cpp
+++ Point.cpp
@@ -2,21 +2,24 @@
 
 #include "Point.h"
 
-std::ostream & operator << (std::ostream &out, const Point &p) {
+std::ostream &operator<<(std::ostream &out, const Point &p){
   out << "(" << p.getX() << ", " << p.getY() << ")";
   return out;
 }
 
-std::istream & operator >> (std::istream &in,  Point &p) {
+std::istream &operator>>(std::istream &in, Point &p){
   in >> p.x;
   in >> p.y;
   return in;
 }
 
-Point Point::operator + (const Point &p) {
-  // TODO: implement this operator.
+Point Point::operator+(const Point &p){
+  double _x = this->getX() + p.getX();
+  double _y = this->getY() + p.getY();
+  return *new Point(_x, _y);
 }
 
-Point& Point::operator += (const Point &p) {
+Point &Point::operator+=(const Point &p)
+{
   // TODO: implement this operator.
 }
