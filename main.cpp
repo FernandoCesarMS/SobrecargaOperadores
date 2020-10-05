e611c5dbbb28e672b4049695358208b7341055c0
Fernando Cesar <51773254+FernandoUFMG@users.noreply.github.com>
2020-10-05 19:09:23 -0300

    Programa Testado e Aprovado

--- main.cpp
+++ main.cpp
@@ -3,44 +3,56 @@
 
 #include "Point.h"
 
-void readPoints(std::vector<Point> &vec) {
+void readPoints(std::vector<Point> &vec)
+{
   Point p;
-  while (std::cin >> p) {
+  while (std::cin >> p)
+  {
     vec.push_back(p);
   }
 }
 
-void printPoints(std::vector<Point> &vec) {
-  for (Point p: vec) {
+void printPoints(std::vector<Point> &vec)
+{
+  for (Point p : vec)
+  {
     std::cout << p << " ";
   }
   std::cout << std::endl;
 }
 
-Point sumAssPoints(std::vector<Point> &vec) {
+Point sumAssPoints(std::vector<Point> &vec)
+{
   Point pSum;
-  for (Point p: vec) {
+  for (Point p : vec)
+  {
     pSum += p;
   }
   return pSum;
 }
 
-Point sumBinPoints(std::vector<Point> &vec) {
+Point sumBinPoints(std::vector<Point> &vec)
+{
   Point pSum;
-  for (Point p: vec) {
+  for (Point p : vec)
+  {
     pSum = pSum + p;
   }
   return pSum;
 }
 
-int main() {
+int main()
+{
   std::vector<Point> vec;
   char testCase;
   std::cin >> testCase;
   readPoints(vec);
-  if (testCase == 'a') {
+  if (testCase == 'a')
+  {
     std::cout << "Sum = " << sumAssPoints(vec) << std::endl;
-  } else if (testCase == 'b') {
+  }
+  else if (testCase == 'b')
+  {
     std::cout << "Sum = " << sumBinPoints(vec) << std::endl;
   }
   printPoints(vec);
